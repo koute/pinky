59f12084d5ea53c9ca458a7fa3f9ac14bdb6fe25
Jan Bujak <j@exia.io>
2016-10-27 23:15:31 +0200

    Add env_logger to development UI

--- pinky-devui/src/main.rs
+++ pinky-devui/src/main.rs
@@ -2,6 +2,7 @@ extern crate sdl2;
 extern crate clock_ticks;
 extern crate serde_json;
 extern crate md5;
+extern crate env_logger;
 
 #[macro_use]
 extern crate emumisc;
@@ -13,6 +14,8 @@ mod renderer;
 mod frame_limiter;
 
 fn main() {
+    env_logger::init().unwrap();
+
     let mut ui = user_interface::create();
     ui.run();
 }
